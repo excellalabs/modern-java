2c937719f64f863825e58ab84aaacefeed823258
Wyn Van Devanter <wynvandev@gmail.com>
2020-12-09 02:40:07 -0500

    cleanup

--- src/main/java/com/excella/java/Application.java
+++ src/main/java/com/excella/java/Application.java
@@ -6,37 +6,25 @@ import com.excella.java.chap11.OptionalMain;
 import com.excella.java.chap12DateTimeApi.DateAndTimeAPIHarness;
 import com.excella.java.stocks.DslHarness;
 
-// @SpringBootApplication
-// public class ReactorApplication {
-
-//     public static void main(final String[] args) throws Exception {
-//         SpringApplication.run(ReactorApplication.class, args);
-//     }
-// }
-
-
-
-//public class Application {
-//
-//	public static void main (String[] args)
-//    {
-//        //ParallelStreamHarness.run();
-//
-//        DslHarness.run();
-//
-//        OptionalMain optionalMain = new OptionalMain();
-//        optionalMain.run();
-//
-//        DateAndTimeAPIHarness.run();
-//    }
-//}
-
 public class Application {
 
     public static void main(String[] args) {
+        //Micronaut.run(Application.class, args);
+
+        ConsoleCode();
+    }
+
+    private static void ConsoleCode() {
+        ParallelStreamHarness.run();
+
+        DslHarness.run();
+
+        OptionalMain optionalMain = new OptionalMain();
+        optionalMain.run();
+
+        DateAndTimeAPIHarness.run();
+
 //        TempInfoSkaffold tempInfoSkaffold = new TempInfoSkaffold();
 //        tempInfoSkaffold.run();
-
-        Micronaut.run(Application.class, args);
     }
 }

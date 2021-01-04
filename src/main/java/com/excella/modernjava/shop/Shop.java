2a6407881e5a3ed16a7d2ce7bd0106a82622d2a6
Wyn Van Devanter <wynvandev@gmail.com>
2021-01-04 15:02:10 -0500

    readme

--- src/main/java/com/excella/modernjava/shop/Shop.java
+++ src/main/java/com/excella/modernjava/shop/Shop.java
@@ -7,7 +7,7 @@ import java.util.concurrent.Future;
 import static com.excella.modernjava.shop.FakeLongRunningOp.delay;
 
 public class Shop {
-    /// ### 16.2.2: Error handling added manually for demonstration, then refactored to helper method below
+    /// ### 16.2.2: Error handling added manually for demonstration, then refactored to CompleteableFuture factory method below
 
     //    public Future<Double> getPriceAsync(String product) {
     //        CompletableFuture<Double> futurePrice = new CompletableFuture<>();

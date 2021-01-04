870ae0bd112385307a882571e62ab1481dcf93f9
Wyn Van Devanter <wynvandev@gmail.com>
2021-01-04 08:53:09 -0500

    ShopClient

---
+++ src/main/java/com/excella/modernjava/shop/ShopClient.java
@@ -0,0 +1,23 @@
+package com.excella.modernjava.shop;
+
+import java.util.concurrent.Future;
+
+public class ShopClient {
+    public static void run() {
+        Shop shop = new Shop();
+        long start = System.nanoTime();
+        Future<Double> futurePrice = shop.getPriceAsync("my favorite product");
+        long invocationTime = ((System.nanoTime() - start) / 1_000_000);
+        System.out.println("Invocation returned after " + invocationTime + " msecs");
+        // Do some more tasks, like querying other shops, // while the price of the product is being calculated
+        //doSomethingElse();
+        try {
+            double price = futurePrice.get();
+            System.out.printf("Price is %.2f%n", price);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        long retrievalTime = ((System.nanoTime() - start) / 1_000_000);
+        System.out.println("Price returned after " + retrievalTime + " msecs");
+    }
+}
